7eb878a661688ad35cbd71a3ac99d159a2c029cb
Bruno Mammana <bruno.mammana.oe@gmail.com>
2024-04-01 15:27:42 -0300

    Work finalizado

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,33 +3,32 @@ import WorkItem from './WorkItem';
 
 const data= [
     {
-        year: 123,
-        title:123,
-        duration: 123,
-        details: 123,
+        year: 2024,
+        title: 'Merola Informatica',
+        duration: 'x months',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     },
     {
-        year: 123,
-        title:123,
-        duration: 123,
-        details: 123,
-    },
-    {
-        year: 123,
-        title:123,
-        duration: 123,
-        details: 123,
-    },
-    {
-        year: 123,
-        title:123,
-        duration: 123,
-        details: 123,
+        year: 2023,
+        title: 'NextDial',
+        duration: '4 months',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     },
 ]
 const Work = () => {
   return (
-    <div>Work</div>
+    <div id='work' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+        <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Work</h1>
+        {data.map((item, idx)=>(
+            <WorkItem 
+                key = {idx} 
+                year = {item.year} 
+                title ={item.title}
+                duration = {item.duration} 
+                details = {item.details}
+            />
+        ))}
+    </div>
   )
 }
 
