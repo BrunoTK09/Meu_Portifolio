dccc049e82938a655da93df12b8e02ed6df31e72
BrunoTk09 <bruno.mammana.oe@gmail.com>
2024-04-05 17:28:48 -0300

    Finished the portfolio (still missing the resume) and idk why the sidebav isn't closing on mobile

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,17 +4,32 @@ const Contact = () => {
   return (
     <div id='contact' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
         <h1 className='py-4 text-4xl font-bold text-center text-[#001b5e]'>Contact</h1>
-        <form action='' method='POST' encType='multipart/form-data'>
-            <div>
-                <div>
-                    <label>Name</label>
-                    <input type="text" name='name' />
+        <form action='https://getform.io/f/pbgxldga' method='POST' encType='multipart/form-data'>
+            <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+                <div className='flex flex-col'>
+                    <label className='uppercase text-sm py2'>Name</label>
+                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name='name' />
                 </div>
-                <div>
-                    <label>Phone Number</label>
-                    <input type="text" name='phone' />
+                <div className='flex flex-col'>
+                    <label className='uppercase text-sm py2'>Phone</label>
+                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name='phone' />
                 </div>
             </div>
+            <div className='flex flex-col py-2'>
+                <label className='uppercase text-sm py2'>Email</label>
+                <input className='border-2 rounded-lg p-3 flex border-gray-300' type="email" name="email" />
+            </div>
+            <div className='flex flex-col py-2'>
+                <label className='uppercase text-sm py2'>Subject</label>
+                <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name="subject" />
+            </div>
+            <div className='flex flex-col py-2'>
+                <label className='uppercase text-sm py2'>Message</label>
+                <textarea className='border-2 rounded-lg p-3 border-gray-300' rows='10' name="message"></textarea>
+            </div>
+            <button className='bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg'>
+                Send Message
+            </button>
         </form>
     </div>
   )
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -13,35 +13,35 @@ const Sidenav = () => {
     const [nav, setNav] = useState(false);
     const handleNav = () => {
         setNav(!nav);
-        console.log('state changed');
-    }
+    };
+
     return (
     <div>
         <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden' />
         {
             nav ? (
                 <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
-                    <a href="#main" 
+                    <a onckick={handleNav} href="#main" 
                        className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <AiOutlineHome size={20} />
                         <span className='pl-4'>Home</span>
                         </a>
-                    <a href="#work" 
+                    <a onckick={handleNav} href="#work" 
                        className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <GrProjects size={20} />
                         <span className='pl-4'>Work</span>
                         </a>
-                    <a href="#projects" 
+                    <a onckick={handleNav} href="#projects" 
                        className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <AiOutlineProject size={20} />
                         <span className='pl-4'>Projects</span>
                         </a>
-                    <a href="#main" 
+                    <a onckick={handleNav} href="#main" 
                        className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <BsPerson size={20} />
                         <span className='pl-4'>Resume</span>
                         </a>
-                    <a href="#contact" 
+                    <a onckick={handleNav} href="#contact" 
                        className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <AiOutlineMail size={20} />
                         <span className='pl-4'>Contact</span>
